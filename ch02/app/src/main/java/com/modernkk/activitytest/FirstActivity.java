4f18b71377ca93747c9de482e8063210ffea19b2
xuke <blk1314@126.com>
2018-09-08 20:56:48 +0800

    add menu

--- ch02/app/src/main/java/com/modernkk/activitytest/FirstActivity.java
+++ ch02/app/src/main/java/com/modernkk/activitytest/FirstActivity.java
@@ -2,6 +2,8 @@ package com.modernkk.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,5 +23,25 @@ public class FirstActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case  R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
