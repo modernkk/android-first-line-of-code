460f9d3a4f5971fb00850fd231517a51ace2286a
xuke <blk1314@126.com>
2018-09-13 00:03:21 +0800

    listview

---
+++ ch03/ListViewTest/app/src/main/java/com/modernkk/listviewtest/FruitAdapter.java
@@ -0,0 +1,33 @@
+package com.modernkk.listviewtest;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+    private int resourceId;
+    public FruitAdapter(@NonNull Context context, int resource, @NonNull List<Fruit> objects) {
+        super(context, resource, objects);
+        resourceId = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        ImageView fruitImage = view.findViewById(R.id.fruit_image);
+        TextView fruitText = view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitText.setText(fruit.getName());
+        return view;
+    }
+}
