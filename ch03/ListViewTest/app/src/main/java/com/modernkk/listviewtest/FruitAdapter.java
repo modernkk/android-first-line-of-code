c234a6f200867205169875ec50b48ae7308e1e62
xuke <blk1314@126.com>
2018-09-13 00:11:56 +0800

    listview

--- ch03/ListViewTest/app/src/main/java/com/modernkk/listviewtest/FruitAdapter.java
+++ ch03/ListViewTest/app/src/main/java/com/modernkk/listviewtest/FruitAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class FruitAdapter extends ArrayAdapter<Fruit> {
     private int resourceId;
+
     public FruitAdapter(@NonNull Context context, int resource, @NonNull List<Fruit> objects) {
         super(context, resource, objects);
         resourceId = resource;
@@ -23,11 +24,27 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Fruit fruit = getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
-        ImageView fruitImage = view.findViewById(R.id.fruit_image);
-        TextView fruitText = view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitText.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+
+    class ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
--- ch03/ListViewTest/app/src/main/java/com/modernkk/listviewtest/MainActivity.java
+++ ch03/ListViewTest/app/src/main/java/com/modernkk/listviewtest/MainActivity.java
@@ -2,8 +2,11 @@ package com.modernkk.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
             "Apple", "Banana", "Orange", "Watermelon", "Pear",
             "Grape", "Pineapple", "Strawberry", "Cherry", "Mango", "Pine"};
     private List<Fruit> fruitList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +26,16 @@ public class MainActivity extends AppCompatActivity {
         FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
+
     private void initFruits() {
         for (int i = 0; i < 2; i++) {
             Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
