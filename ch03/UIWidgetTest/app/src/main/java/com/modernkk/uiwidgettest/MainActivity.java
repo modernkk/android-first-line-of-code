690fa52e14892b705a3e52e412bf7beeffb5fe92
xuke <blk1314@126.com>
2018-09-12 22:31:25 +0800

    title layout

--- ch03/UIWidgetTest/app/src/main/java/com/modernkk/uiwidgettest/MainActivity.java
+++ ch03/UIWidgetTest/app/src/main/java/com/modernkk/uiwidgettest/MainActivity.java
@@ -1,28 +1,20 @@
 package com.modernkk.uiwidgettest;
 
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.media.Image;
-import android.support.v7.app.AlertDialog;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private EditText editText;
-    private ImageView imageView;
-    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
     }
 }
