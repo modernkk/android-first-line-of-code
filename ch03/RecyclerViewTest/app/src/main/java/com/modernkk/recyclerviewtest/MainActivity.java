611562ed78a037eebc16005a0f4cf8e6a88e876c
xuke <blk1314@126.com>
2018-09-13 23:35:29 +0800

    StaggeredGridLayoutManager

--- ch03/RecyclerViewTest/app/src/main/java/com/modernkk/recyclerviewtest/MainActivity.java
+++ ch03/RecyclerViewTest/app/src/main/java/com/modernkk/recyclerviewtest/MainActivity.java
@@ -4,20 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private List<Fruit> fruitList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initFruits();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(
+                3, StaggeredGridLayoutManager.VERTICAL
+        );
         recyclerView.setLayoutManager(layoutManager);
         FruitAdapter adapter = new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
@@ -25,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void initFruits() {
         for (int i = 0; i < 2; i++) {
-            Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic);
             fruitList.add(apple);
-            Fruit banana = new Fruit("banana", R.drawable.banana_pic);
+            Fruit banana = new Fruit(getRandomLengthName("banana"), R.drawable.banana_pic);
             fruitList.add(banana);
             Fruit orange = new Fruit("orange", R.drawable.orange_pic);
             fruitList.add(orange);
@@ -46,5 +50,15 @@ public class MainActivity extends AppCompatActivity {
             Fruit mango = new Fruit("mango", R.drawable.mango_pic);
             fruitList.add(mango);
         }
+    }
+
+    private String getRandomLengthName(String name) {
+        Random random = new Random();
+        int length = random.nextInt(20) +1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(name);
+        }
+        return builder.toString();
     }
 }
